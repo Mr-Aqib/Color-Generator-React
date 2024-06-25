55140ef16af45eab4d58162168f76ba5d1207b19
Aqib1422 <mraqibshafique@gmail.com>
2024-06-26 00:43:03 +0500

    Color Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ColorGene from './color-Gen/ColorGene'
-
+import toast, { Toaster } from 'react-hot-toast';
+import "./color-Gen/style.css"
 const App = () => {
   return (
     <>
-      <ColorGene/>
+      <ColorGene />
+      <Toaster/>
     </>
   )
 }
--- src/color-Gen/ColorGene.jsx
+++ src/color-Gen/ColorGene.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 import Values from 'values.js'
 const ColorGene = () => {
     const [input, setInput] = useState("")
+    const [err,setErr] = useState(false)
     const handleChange = (e) =>
     { 
         e.preventDefault();
-        const newcolor = new Values(input).all(10)
-        console.log(newcolor)
+        try {
+            setErr(false)
+            const newcolor = new Values(input).all(10)
+            toast.success("Color Generated")
+            console.log(newcolor)
+            setInput("")
+        }
+        catch
+        {
+            setErr(true)
+            toast.error("Invalid Color")
+            console.log("Invalid Color")
+        }
         }
   return (
       <>
           <div className="container">
-              <div style={{background:'rgb(245,214,100)'}} className="col-lg-5 my-3 rounded-4 mx-auto shadow p-3">
+              <div  className="search-con col-lg-5 my-3 rounded-4 mx-auto shadow p-3">
                   <form action="">
                       <label className='fs-3 text-center d-block' htmlFor="">Color Generator</label>
-                      <input onChange={(e)=> setInput(e.target.value)} value={input} placeholder='e.g .brown' className='form-control' type="text" name="" id="" />
+                      <input style={{border :` ${err? '2px solid red' : "" }`}} onChange={(e)=> setInput(e.target.value)} value={input} placeholder='e.g .brown' className='form-control' type="text" name="" id="" />
                       <button onClick={handleChange} className="btn my-3 btn-danger w-50 d-block mx-auto">
                           Generate
                       </button>
